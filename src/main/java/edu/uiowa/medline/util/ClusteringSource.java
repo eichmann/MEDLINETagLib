55125cead71b2062ec42c75ecb7b34442057fb22
eichmann <eichmann@deep-thought.info-science.uiowa.edu>
2011-09-30 14:50:36 +0000

    MEDLINE plugin now generating clusters in the new scheme.

--- src/main/java/edu/uiowa/medline/util/ClusteringSource.java
+++ src/main/java/edu/uiowa/medline/util/ClusteringSource.java
@@ -1,10 +1,12 @@
 package edu.uiowa.medline.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Hashtable;
+import java.util.Properties;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,11 +24,36 @@ public class ClusteringSource extends ExternalSource {
 
     Hashtable<String, Instance> pmidHash = new Hashtable<String, Instance>();
 
-    public void generateClusters(Vector<Cluster> clusters, Author author) throws NamingException, SQLException {
+    public void generateClusters(Vector<Cluster> clusters, Author author) {
+    	try {
+			cluster(clusters, author);
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    void cluster(Vector<Cluster> clusters, Author author) throws NamingException, SQLException, ClassNotFoundException {
 		logger.debug(label + " clustering: " + author);
 
-	    DataSource theDataSource = (DataSource)new InitialContext().lookup("java:/comp/env/jdbc/MEDLINETagLib");
-	    Connection theConnection = theDataSource.getConnection();
+		Connection theConnection = null;
+	    if (tomcat) {
+	    	DataSource theDataSource = (DataSource)new InitialContext().lookup("java:/comp/env/jdbc/MEDLINETagLib");
+	    	theConnection = theDataSource.getConnection();
+	    } else {
+	        Class.forName("org.postgresql.Driver");
+			Properties props = new Properties();
+			props.setProperty("user", "eichmann");
+			props.setProperty("password", "translational");
+			props.setProperty("sslfactory", "org.postgresql.ssl.NonValidatingFactory");
+			props.setProperty("ssl", "true");
+			theConnection = DriverManager.getConnection("jdbc:postgresql://neuromancer.icts.uiowa.edu/bioinformatics", props);
+	    	
+	    }
 		Pattern medDatePattern = Pattern.compile("^([0-9][0-9][0-9][0-9])(-[0-9][0-9][0-9][0-9])? ?.*");
         PreparedStatement stmt = theConnection.prepareStatement("select author.pmid,pub_year,article.title,medline_date from medline11.author,medline11.journal, medline11.article where journal.pmid=article.pmid and article.pmid=author.pmid and last_name = ? and fore_name = ? order by pmid desc");
         stmt.setString(1,author.getLastName());
